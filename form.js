d214f8bce3ecee1a3700e5f953a35a04bfcec25f
MakenaG <makenaglory44@gmail.com>
2022-12-21 20:18:18 +0300

    Done

--- form.js
+++ form.js
@@ -1,17 +1,23 @@
-const loginText = document.querySelector(".title-text .login");
-         const loginForm = document.querySelector("form.login");
-         const loginBtn = document.querySelector("label.login");
-         const signupBtn = document.querySelector("label.signup");
-         const signupLink = document.querySelector("form .signup-link a");
-         signupBtn.onclick = (()=>{
-           loginForm.style.marginLeft = "-50%";
-           loginText.style.marginLeft = "-50%";
-         });
-         loginBtn.onclick = (()=>{
-           loginForm.style.marginLeft = "0%";
-           loginText.style.marginLeft = "0%";
-         });
-         signupLink.onclick = (()=>{
-           signupBtn.click();
-           return false;
-         });
+//TODO : Its a Completed Code//
+function formvalid() {
+  var vaildpass = document.getElementById("pass").value;
+
+  if (vaildpass.length <= 8 || vaildpass.length >= 20) {
+    document.getElementById("vaild-pass").innerHTML = "Minimum 8 characters";
+    return false;
+  } else {
+    document.getElementById("vaild-pass").innerHTML = "";
+  }
+}
+function show() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+      x.type = "text";
+      document.getElementById("showimg").src =
+        "https://static.thenounproject.com/png/777494-200.png";
+    } else {
+      x.type = "password";
+      document.getElementById("showimg").src =
+        "https://cdn2.iconfinder.com/data/icons/basic-ui-interface-v-2/32/hide-512.png";
+    }
+  }
